dbde45594f07828e50b67b6780c6fd9509ed8c13
tobisor <or.tobis@gmail.com>
2017-03-27 01:12:09 +0300

    score manager

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+    //public GameObject obj;
+    public int multiFactor;
+    public int score;
+    float altAtStart;
+    private Vector3 startingPosition;
+    public Text scoreText;
+
+    // Use this for initialization
+    void Start () {
+        score = 0;
+        startingPosition = transform.localPosition;
+        altAtStart = startingPosition.y;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //score += (int)(transform.obj.y - altAtStart);
+		scoreText.text = "Score: " + Mathf.Round(score);
+    }
+}
