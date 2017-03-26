cf41c3cf9b4ac956ae6e2e5eff99a5b0c2f17642
tobisor <or.tobis@gmail.com>
2017-03-26 18:58:11 +0300

    timer
    1. timer is working
    2. adding time to timer while looking at object doesnt work yet

--- Assets/GoogleVR/Demos/Scripts/GVRDemo/Teleport.cs
+++ Assets/GoogleVR/Demos/Scripts/GVRDemo/Teleport.cs
@@ -17,11 +17,13 @@ using System.Collections;
 
 [RequireComponent(typeof(Collider))]
 public class Teleport : MonoBehaviour {
-  private Vector3 startingPosition;
-
-  public Material inactiveMaterial;
-  public Material gazedAtMaterial;
-
+    private Vector3 startingPosition;
+    
+    public Material inactiveMaterial;
+    public Material gazedAtMaterial;
+    public GameObject timer;
+    float timeFactor;
+    public float timeBonus;
   void Start() {
     startingPosition = transform.localPosition;
     SetGazedAt(false);
@@ -34,7 +36,11 @@ public class Teleport : MonoBehaviour {
             GetComponent<Renderer>().material = gazedAt ? gazedAtMaterial : inactiveMaterial;
             return;
         }
-        GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+        GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;      
+        timer.GetComponent<Timer>().timeLeft += gazedAt?  timeBonus : 0f ;
+        
+
+
     }
 
     public void Reset() {
---
+++ Assets/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+    public float timeLeft = 60.0f;
+    public Text timerText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeLeft -= Time.deltaTime;
+        timerText.text = "Time Left:" + Mathf.Round(timeLeft);
+        
+        if (timeLeft < 0f)
+        {
+
+        }
+    }
+
+    public void addTimer (float x)
+    {
+        timeLeft += x;
+    }
+    
+}
