b0fdbb83d1ad0f843222ad5326b2ab1e75320898
tobisor <or.tobis@gmail.com>
2017-05-12 14:36:19 +0300

    stats manager

---
+++ Assets/Scripts/StatsManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class StatsManager : MonoBehaviour {
+    public float timeFromStart , lastRotationTime;
+    public int cameraYPosAtStart, cameraYpos;
+    public float maxRotationTime, tempRotationTime;
+    bool rotationFinished ;
+
+    public float speed, maxSpeed;
+    Vector3 lastPosition;
+
+    public int maxCombo;
+	// Use this for initialization
+	void Start () {
+        maxRotationTime = float.MaxValue;
+        timeFromStart = 0f;
+        lastRotationTime = 0f;
+        cameraYPosAtStart =(int) ((transform.parent.GetComponent<Transform>().rotation.y + 180) % 360);
+        rotationFinished = false;
+        
+        lastPosition = GetComponent<Transform>().position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeFromStart += Time.deltaTime;
+        
+        
+        calcRotationTime();
+        updateMaxCombo();
+        updateMaxSpeed();
+        lastPosition = GetComponent<Transform>().position;
+        maxSpeed = 0f;
+
+    }
+    
+    
+    private void calcRotationTime() {
+        cameraYpos =(int) ((transform.parent.GetComponent<Transform>().rotation.y + 180) % 360);
+         
+        if ((cameraYPosAtStart == cameraYpos))
+        {
+            if (rotationFinished)
+            {
+                tempRotationTime = timeFromStart - lastRotationTime;
+                lastRotationTime = timeFromStart;
+                Debug.Log("last rotation" + tempRotationTime);
+                if (tempRotationTime < maxRotationTime)
+                {
+                    maxRotationTime = tempRotationTime;
+                    Debug.Log("max rotation" + maxRotationTime);
+                }
+                rotationFinished = false;
+            }
+            
+        } else
+        {
+            rotationFinished = true;
+        }
+    }
+
+    private void updateMaxCombo()
+    {
+        maxCombo = maxCombo < ComboManager.ComboLength ? ComboManager.ComboLength : maxCombo;
+    }
+
+    private void updateMaxSpeed()
+    {
+        speed = (GetComponent<Transform>().position - lastPosition).magnitude / Time.deltaTime;
+        if (speed > maxSpeed)
+        {
+            maxSpeed = speed;
+            //Debug.Log("s[eed: " + speed);
+        }
+    }
+}
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour {
-    public float timeLeft = 20.0f;
+    public float timeFromStart, timeLeft = 20.0f;
     public Text timerText;
+
 
 	// Use this for initialization
 	void Start () {
-		
+        timeFromStart = 0;
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
         timeLeft -= Time.deltaTime;
+        timeFromStart += Time.deltaTime;
         string minSec = (timeLeft > 15.0f) ? string.Format("{0}:{1:00}", (int)timeLeft / 60, (int)timeLeft % 60) :
             ((timeLeft > 0) ? System.Math.Round(timeLeft, 2).ToString() : "0.00");
         timerText.text = minSec;
