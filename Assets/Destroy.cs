2eab3a9c78a4d6fafae3d9ffe09ce6ed472f4e9e
Kedeman <kedeman@hotmail.com>
2017-07-08 19:32:47 +0300

    Animation_WAR

---
+++ Assets/Destroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+    public float time = 5;
+    // Use this for initialization
+    void Start () {
+    }
+	// Update is called once per frame
+	void Update () {
+        Destroy(gameObject, time);
+    }
+}
