2f55d884aff8290db3f0c95746e5138d90b7dfb7
idowolf <idowolf@gmail.com>
2017-07-08 13:34:33 +0300

    Move dynamically to target

---
+++ Assets/newrotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newrotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 0, Time.deltaTime * 0.1f)); // move forward
+        transform.Rotate(new Vector3(0, Time.deltaTime * 0.1f, 0)); // turn a little
+
+    }
+
+}
